173a8f4426356f254543db1f429c7d92efc1c604
marvin-digipen <marvin.chan@digipen.edu>
2019-11-07 13:39:29 -0800

    Add PlatformController

---
+++ PongProject/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    public float speed = 5;
+    public KeyCode DownKey;
+    public KeyCode UpKey;
+    Rigidbody2D platform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platform = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(UpKey))
+        {
+            platform.MovePosition(platform.position + new Vector2(0, speed) * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey(DownKey))
+        {
+            platform.MovePosition(platform.position + new Vector2(0, -speed) * Time.fixedDeltaTime);
+        }
+    }
+}
