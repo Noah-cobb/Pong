ede28d32caae0d75094714587b779ee84b91982f
Noah Cobb <Noah.Cobb@DigiPen.edu>
2019-11-07 13:13:44 -0800

    First Unity File

---
+++ PongProject/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string NextScene = "Level 1";
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(NextScene);
+
+    }
+
+
+
+    void Start()
+    {
+        
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
