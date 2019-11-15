dcffb5a1c400697f594f2eeb0ca299033b8d05a0
marvin-digipen <marvin.chan@digipen.edu>
2019-11-15 13:42:23 -0800

    move audio folder, audio script

--- PongProject/Assets/Sound effects/SoundWhilePlaying.cs
+++ PongProject/Assets/Sound effects/SoundWhilePlaying.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SoundWhilePlaying : MonoBehaviour
 {
+    private AudioSource source;
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
+        source.Play();
     }
 
     // Update is called once per frame
