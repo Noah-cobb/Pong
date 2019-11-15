f4e63f67dcde8a69c8c1b1ec2595d577aeb32821
Noah Cobb <Noah.Cobb@DigiPen.edu>
2019-11-15 13:12:45 -0800

    Sound scripts

--- PongProject/Assets/Sound effects/SoundOnImpact.cs
+++ PongProject/Assets/Sound effects/SoundOnImpact.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class SoundOnImpact : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private AudioSource audioSource;
+    public AudioClip Scored;
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag == "Goal")
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.clip = Scored;
+            audioSource.Play();
+        }
     }
 }
